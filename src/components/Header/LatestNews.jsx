4e5b4a34bb2821e147664d4bc3cb70ce175341c8
Al-amin <muhammadalamin809@gmail.com>
2026-04-04 17:27:43 +0800

    show breaking news in header with marquee

--- src/components/Header/LatestNews.jsx
+++ src/components/Header/LatestNews.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
 
 const LatestNews = () => {
+  const [latestNews, setLatestNews] = useState([]);
+  
+  useEffect(() => {
+    fetch("/news.json")
+    .then(res => res.json())
+    .then(data => {
+      const breakingNews = data.filter(news => news?.others?.is_today_pick === true)
+      setLatestNews(breakingNews)
+    })
+  }, [])
+  
   return (
     <div className='flex items-center gap-3 bg-base-200 p-3'>
         <p className='bg-secondary text-base-100 px-6 py-2'>Latest</p>
-      <Marquee className='flex items-center font-semibold gap-5' pauseOnHover={true} speed={60}>
-        <p>Match Highlights: Germany vs Spain — as it happened! Match Highlights: Germany vs Spain as.</p>
-        <p>Match Highlights: Germany vs Spain — as it happened! Match Highlights: Germany vs Spain as.</p>
-        <p>Match Highlights: Germany vs Spain — as it happened! Match Highlights: Germany vs Spain as.</p>
+      <Marquee className='' pauseOnHover={true} speed={60}>
+       {latestNews.map(news => <p className='font-semibold mr-3'>{news?.title}.</p>)}
       </Marquee>
 
     </div>
